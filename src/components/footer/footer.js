30c27d6d757466c853201c4c94e4566d7f86ce14
franc51 <101558212+franc51@users.noreply.github.com>
2022-07-24 12:38:55 +0300

    footer 1st commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './footer.scss'
+
+export default function Footer(){
+    return (
+        <footer>
+            <p>&#169; 2022 Novabit, a project by Szasz Francisc and Sebastian Onofrei.</p>
+        </footer>
+    )
+}
