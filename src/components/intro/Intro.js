6cf1c9e84c180f43143f60bfc615b83dbc308fd0
Sebastian <sonofra98@gmail.com>
2022-07-24 11:51:01 +0300

    created mockup , for text intro div

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import "./intro.scss"
 
 export default function Intro() {
   return (
-    <div>Intro</div>
+    <div className='intro'>
+      {/* Aici o sa schimbam textul cu ceva mai creativ
+          Acum doar facem o schita a siteului,nu varianta finala.
+        */}
+      <h4>Simplicity</h4>
+      <h4>Efficiency</h4>
+      <h4>Performance</h4>
+    </div>
   )
 }
