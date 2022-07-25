7abcf86fa99ce0597a5f1d4167ef118ec091dd5c
franc51 <101558212+franc51@users.noreply.github.com>
2022-07-25 17:41:50 +0300

    features 2nd commit

--- src/components/features123/features123.js
+++ src/components/features123/features123.js
@@ -5,14 +5,16 @@ import './features123.scss';
 
 export default function Features123(){
     return(
-        <><div className="features2">
-            <div className="buc2"><h2>High-Fidelity</h2></div>
-            <div className="buc2"><h2>High-Power</h2></div>
-            <div className="buc2"><h2>High-Efficiency</h2></div>
-        </div><div className="features">
-                <div className="buc"><img src={require('./loud-speaker.png')} alt="High-Fidelity" /></div>
-                <div className="buc"><img src={require('./fuse-box.png')} alt="High-Power" /></div>
-                <div className="buc"><img src={require('./renewable-energy.png')} alt="Efficient" /></div>
+        <><div className="features_names">
+            <div className="feature_name"><h2>High-Fidelity</h2></div>
+            <div className="feature_name"><h2>High-Power</h2></div>
+            <div className="feature_name"><h2>High-Efficiency</h2></div>
+        </div>
+        
+        <div className="features_picture">
+                <div className="pictures"><img src={require('./loud-speaker.png')} alt="High-Fidelity" /></div>
+                <div className="pictures"><img src={require('./fuse-box.png')} alt="High-Power" /></div>
+                <div className="pictures"><img src={require('./renewable-energy.png')} alt="Efficient" /></div>
             </div></>
     )
 }
